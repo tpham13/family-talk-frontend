e6a4428ad87cb92073b9e27eb6818b397af089b4
tpham13 <tpham0123@gmail.com>
2021-02-19 11:46:18 -0800

    Created store.js file and moved everything store related from index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk  from 'redux-thunk';
 import { Provider } from 'react-redux';
-import usersReducer from './reducers/users.js';
+import store from './store';
 
-const reducer = combineReducers({
-  user: usersReducer
-})
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 ReactDOM.render(
     <Provider store={ store }>
         <App />
