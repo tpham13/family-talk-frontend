e411f85df7366d0aed9187f037c73626ad6c3c09
tpham13 <tpham0123@gmail.com>
2021-03-01 14:00:38 -0800

    Added UI to PostCard

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 
 // PostCard is expect post as a props so we need to pass post as a props in App.js route
 const PostCard = ({post}) => {
 
     return (
-        post ?
-        <div className="PostCard" >
-            <ul>{post.attributes.content} shared by {post.attributes.user.name}</ul>
-            <Link to={`/posts/${post.id}/edit`}>Edit this post</Link>
-            {/* <button className="edit">Edit</button> */}
-        </div> :
+        <div className="PostCard">
+       { post ? (
+            <Card> 
+                <CardMedia style={{height: 0, paddingTop: '10.25%'}}
+                    
+                    />
+                <CardContent> 
+                    <Typography gutterBottom variant="headline" component="h2">
+                        <p>{post.attributes.content} shared by {post.attributes.user.name} </p>
+                        
+                    </Typography>
+                </CardContent>
 
-       <p>THis is the post card</p>
+                <CardActions> 
+                    <Button size="small" color="primary">
+                        <Link to={`/posts/${post.id}/edit`}>Edit this post</Link>
+                    </Button>
+                </CardActions>
+               
+       
+            </Card>
+        )  :<p>This is the post card</p> }
+
+        </div>
     )
 }
 
