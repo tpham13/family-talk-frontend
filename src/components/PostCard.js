358e7a174f6204117fded164ae036a7199a2510a
tpham13 <tpham0123@gmail.com>
2021-03-01 22:02:22 -0800

    PostCard display in card UI style

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -3,15 +3,19 @@ import {Link} from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
+// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+
+
+
+
 const useStyles = makeStyles({
     root: {
-      maxWidth: 345,
+      maxWidth: 345, 
     },
    
   });
@@ -26,7 +30,7 @@ const PostCard = ({post}) => {
                 <CardMedia
                     component="img"
                     alt="Contemplative Reptile"
-                    height="140"
+                    height="100"
                     image="https://lh3.googleusercontent.com/proxy/saBhOVeMuZWCDo2dDHTAbKqJAU2jxk8sX2sRjcp7Bv-R9Pw-5X2F3ZniIEYiC5Wb3c4Pq4cu4RRlanGNYM6oml_iY4IeTDSuf44IoSLiJq0Nt_Zi_LJJwqLoOYKnM8PD3zaQN6b523WYuTGEvgEUn9c9TLx5Ei-1Og4ktdA0GXeTydqIlCfLakNHs-9KHVhxZ_3gevUntNxtReteFAU_M-fouaDr"
                     title="Contemplative Reptile"
                  
@@ -42,11 +46,11 @@ const PostCard = ({post}) => {
                     </Typography>
                 </CardContent>
             </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary">
+            {/* <CardActions> */}
+                <Button size="small" color="primary" href="#contained-buttons">
                     <Link to={`/posts/${post.id}/edit`}>Edit this post</Link>
                 </Button>     
-            </CardActions>
+            {/* </CardActions> */}
     </Card>
     ) :<p>This is the post card</p> );
 
